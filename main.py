7a4c2e13362ca7bb6492bd3665ed7faddccd2d7c
Ar5hv1r <arshvirt2@gmail.com>
2022-07-01 20:28:43 +0100

    recursive backtracking algorithm working

--- main.py
+++ main.py
@@ -13,12 +13,64 @@ board = [
     [0, 9, 0, 0, 0, 0, 4, 0, 0],
 ]
 
-
+# outputs the row and column of each blank square
 def find_blank(board):
     for row in range(len(board)):
         for col in range(len(board[0])):
             if board[row][col] == 0:
-                print(f"Blank at row: {row} and column: {col}")
+                # print(f"Blank at row: {row} and column: {col}")
+                return row, col
 
 
-find_blank(board)
+# outputs current state of the board
+def print_board(board):
+    pp.pprint(board)
+
+
+def solve_puzzle(board):
+    blank = find_blank(board)
+    if not blank:
+        return True
+    else:
+        row, col = blank
+
+    for i in range(1, 10):
+        if check_valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve_puzzle(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+
+def check_valid(board, num, pos):
+    # check if number can fit in column
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # check if number can fit in row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # check if number can fit in box
+    xBox = pos[1] // 3
+    yBox = pos[0] // 3
+
+    for i in range(yBox * 3, yBox * 3 + 3):
+        for j in range(xBox * 3, xBox * 3 + 3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+
+print_board(board)
+
+solve_puzzle(board)
+
+print_board(board)
